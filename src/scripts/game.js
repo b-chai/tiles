d51cfadbc92fae6e070d20c7732294e17b46544c
b-chai <b.chai82@gmail.com>
2021-11-02 01:11:51 -0400

    add button functionality

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -20,17 +20,11 @@ class Game{
 
             let rowArray = []
             for (let j = 0 ; j < col ; j++ ){
-                const tile = new Tile()
+                let tile = new Tile()
 
-                if (i === 0 && j === 0){
-                    let startPiece = tile.playerTile()
-                    seperator.appendChild(startPiece)
-                    rowArray.push(tile)
-                }else{
-                    let piece = tile.randomTile()
-                    seperator.appendChild(piece)
-                    rowArray.push(tile)
-                }
+                let piece = tile.randomTile(i,j)
+                seperator.appendChild(piece)
+                rowArray.push(tile)
             }
             gridArray.push(rowArray)
         }
@@ -49,11 +43,35 @@ class Game{
         button1.className = 'red'
         button1.onclick = ()=>{
             playerTile.color = "red"
+            const currentTile = document.querySelector(`.pos-${playerTile.pos[0]}-${playerTile.pos[1]}`)
+            currentTile.classList.remove("blue", "green", "orange")
+            currentTile.classList.add("red")
             console.log(playerTile)
         }
         button2.className = 'blue'
+        button2.onclick = ()=>{
+            playerTile.color = "blue"
+            const currentTile = document.querySelector(`.pos-${playerTile.pos[0]}-${playerTile.pos[1]}`)
+            currentTile.classList.remove("red", "green", "orange")
+            currentTile.classList.add("blue")
+            console.log(playerTile)
+        }
         button3.className = 'green'
+        button3.onclick = ()=>{
+            playerTile.color = "green"
+            const currentTile = document.querySelector(`.pos-${playerTile.pos[0]}-${playerTile.pos[1]}`)
+            currentTile.classList.remove("blue", "red", "orange")
+            currentTile.classList.add("green")
+            console.log(playerTile)
+        }
         button4.className = 'orange'
+        button4.onclick = ()=>{
+            playerTile.color = "orange"
+            const currentTile = document.querySelector(`.pos-${playerTile.pos[0]}-${playerTile.pos[1]}`)
+            currentTile.classList.remove("blue", "green", "red")
+            currentTile.classList.add("orange")
+            console.log(playerTile)
+        }
 
         selector.appendChild(button1)
         selector.appendChild(button2)
--- src/scripts/tile.js
+++ src/scripts/tile.js
@@ -2,21 +2,24 @@ class Tile{
     constructor(){
         const TILES = ['red','blue','green','orange']
         const randomColor = Math.floor(Math.random() * TILES.length)
+        
         this.color = TILES[randomColor];
         this.player = false
+        this.pos = []
     }
 
-    playerTile(){
-        this.player = true
-        
-        let tile = document.createElement("div")
-        tile.className = `tile ${this.color}`
-        return tile
-    }
 
-    randomTile(){
+    randomTile(x,y){
         let tile = document.createElement("div")
-        tile.className = `tile ${this.color}`
+        tile.className = `tile ${this.color} pos-${x}-${y}`
+
+        this.pos = [x,y]
+
+        // starting position
+        if(x === 0 && y == 0){
+            this.player = true
+        }
+        console.log(tile)
         return tile
     }
 }
